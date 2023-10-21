085f3c512f1712ba0133ca0c64f6a54438bedb68
Keith Thoong <thoonk78@uwosh.edu>
2023-10-20 22:17:20 -0500

    uPdated

--- Assignment 2/PartB.cs
+++ Assignment 2/PartB.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Reflection.Metadata.Ecma335;
 
 namespace Assignment_2;
 
@@ -15,45 +16,60 @@ class PartB
 
         BinarySearchTime(times, 0, times.Count - 1);
     }
-    public static int CompareTimes(String goal, String time)
+    public static int CompareTimes(String time1, String time2)
     {
-        String[] splitGoal = goal.Split(':');
-        String[] splitTime = time.Split(":");
+        String[] time1Full = time1.Split(':');
+        String[] time2Full = time2.Split(":");
 
         for (int i = 0; i < 3; i++)
         {
-            int goalTime = int.Parse(splitGoal[i]);
-            int timeTime = int.Parse(splitTime[i]);
+            int time1Time = int.Parse(time1Full[i]);
+            int time2Time = int.Parse(time2Full[i]);
 
-            if (timeTime < goalTime)
+            if (time1Time < time2Time)
                 return -1;
-            else if (timeTime >= goalTime)
+            else if (time1Time >= time2Time)
                 return 1;
         }
 
-        return 1;
+        return -1;
     }
 
     public static void BinarySearchTime(List<String> list, int start, int end)
     {
-        int mid = start + (end - start) / 2;
-        if (start == end)
+        int mid = (end + start) / 2;
+        
+        if (mid == end)
         {
-            if (CompareTimes(list[1], list[mid]) == -1)
+            if ((end - 2) == int.Parse(list[0]))
             {
-                if ((mid - 2) == int.Parse(list[0]))
-                {
-                    Console.WriteLine("OK");
-                }
-                else
-                {
-                    Console.WriteLine("Problem");
-                }
+                Console.WriteLine("OK");
             }
+            else
+            {
+                Console.WriteLine("Problem");
+            }
+
+            return;
         }
-        else if (CompareTimes(list[1], list[mid]) == -1)
+        else if (mid == start)
         {
-            BinarySearchTime(list, start, mid);
+            Console.WriteLine("Problem");
+            return;
+        }
+        else if (CompareTimes(list[mid], list[1]) == -1 && CompareTimes(list[mid + 1], list[1]) == 1)
+        {
+            if ((mid - 2) == int.Parse(list[0]))
+            {
+                Console.WriteLine("OK");
+            }
+
+            Console.WriteLine("Problem");
+            return;
+        }
+        else if (CompareTimes(list[mid], list[1]) == 1)
+        {
+            BinarySearchTime(list, start, mid - 1);
         }
         else
         {
@@ -62,5 +78,5 @@ class PartB
     }
 
 
-    
+
 }
