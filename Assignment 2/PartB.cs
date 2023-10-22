0567707f69d889f9d205d1317b0518484d52a1e0
tylerkamholz <kamholz.tyler@gmail.com>
2023-10-22 14:55:27 -0500

    Added option menu and comments.

--- Assignment 2/PartB.cs
+++ Assignment 2/PartB.cs
@@ -10,12 +10,15 @@ namespace Assignment_2;
 
 class PartB
 {
-    public static void Run()
+    //main run
+    public void Run()
     {
         List<String> times = File.ReadAllLines("../../../input2.txt").ToList();
 
         BinarySearchTime(times, 0, times.Count - 1);
     }
+
+    //this is the compare method for the 2 times.
     public static int CompareTimes(String time1, String time2)
     {
         String[] time1Full = time1.Split(':');
@@ -35,12 +38,15 @@ class PartB
         return -1;
     }
 
+    //this is the recursive DnC method
     public static void BinarySearchTime(List<String> list, int start, int end)
     {
+        //calculating the mid of the list
         int mid = start + (end - start) / 2;
         
         if (mid >= end)
         {
+            //if exactly the amout return ok, else problem
             if ((end - 2) == int.Parse(list[0]))
             {
                 Console.WriteLine("OK");
@@ -53,11 +59,13 @@ class PartB
 
             return;
         }
+        //if it gets to the start of the list, problem
         else if (mid <= start)
         {
             Console.WriteLine("Problem");
             return;
         }
+        //seeing if the time to the left is lower and the time to the right is higher
         else if (CompareTimes(list[mid], list[1]) == -1 && CompareTimes(list[mid + 1], list[1]) == 1)
         {
             if ((mid - 1) == int.Parse(list[0]))
@@ -69,10 +77,12 @@ class PartB
             Console.WriteLine("Problem");
             return;
         }
+        //traverse on left side
         else if (CompareTimes(list[mid], list[1]) == 1)
         {
             BinarySearchTime(list, start, mid);
         }
+        //traverse on right side
         else
         {
             BinarySearchTime(list, mid, end);
--- Assignment 2/Program.cs
+++ Assignment 2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Assignment_2;
 
@@ -11,7 +12,55 @@ class Program
         PartB partB = new();
         PartD partD = new();
 
-        partD.Run();
+
+        //Option menu
+        prints();
+
+        char option;
+
+        //makes an input a char
+        char.TryParse(Console.ReadLine(), out option);
+        option = char.ToLower(option);
+
+        //this starts the option program
+        while (option != 'q')
+        {
+            switch (option)
+            {
+
+                case 'b':
+                    Console.WriteLine();
+                    partB.Run();
+                    prints();
+                    char.TryParse(Console.ReadLine(), out option);
+                    option = char.ToLower(option);
+                    break;
+
+                case 'c':
+                    Console.WriteLine();
+                    partC.Run();
+                    prints();
+                    char.TryParse(Console.ReadLine(), out option);
+                    option = char.ToLower(option);
+                    break;
+
+                case 'd':
+                    Console.WriteLine();
+                    partD.Run();
+                    prints();
+                    char.TryParse(Console.ReadLine(), out option);
+                    option = char.ToLower(option);
+                    break;
+            }
+        }
+
+    }
+
+    static void prints()
+    {
+        Console.WriteLine();
+        Console.WriteLine("Enter what part of the assignment to test: 'B', 'C', or 'D'. (Q to quit)");
+        Console.Write("Choice: ");
     }
 
 }
